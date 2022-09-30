6a2f39c0974bb25a99c2349874a8c94529a4afcc
Kayden Harrell <110700997+ACupOfLinux@users.noreply.github.com>
2022-09-29 22:32:50 -0400

    Add files via upload

---
+++ Quiz/Program.cs
@@ -0,0 +1,111 @@
+﻿
+using System;
+{
+Console.WriteLine("First question");
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+Console.Beep();
+}
+
+Console.WriteLine("-------------------------- \n " + "Who should you vote for president in 2024 \n " + "A: Donald Trump \n " + "B: Brent Peterson \n " + "C: Joe Biden \n " + "D: A Cup Of Linux \n " + "-------------------------- \n ");
+String answer1 = Console.ReadLine();
+
+if (answer1 == "B")
+{
+    Console.WriteLine("-------------------------- \n " + "Correct \n" + "-------------------------- \n ");
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+    Console.Beep();
+}
+
+else
+{
+    Console.WriteLine("-------------------------- \n " + "Incorrect \n" + "-------------------------- \n ");
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+    Console.Beep();
+}
+
+{
+    Console.WriteLine("Next question");
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+    Console.Beep();
+}
+
+    Console.WriteLine("-------------------------- \n " + "What does C# stand for \n " + "A: C Object Orianted Language\n " + "B: C++++ \n " + "C: Java for jews \n " + "D: Slower but better \n " + "-------------------------- \n ");
+    String answer2 = Console.ReadLine();
+
+    if (answer2 == "A")
+    {
+        Console.WriteLine("-------------------------- \n " + "Correct \n" + "-------------------------- \n ");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+        Console.Beep();
+    }
+
+   if (answer2 == "B")
+    {
+        Console.WriteLine("-------------------------- \n " + "Correct \n" + "-------------------------- \n ");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+        Console.Beep();
+    }
+
+    if (answer2 == "D")
+    {
+        Console.WriteLine("-------------------------- \n " + "Correct \n" + "-------------------------- \n ");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+        Console.Beep();
+    }
+
+    if (answer2 == "C")
+    {
+        Console.WriteLine("-------------------------- \n " + "Correct \n" + "-------------------------- \n ");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+        Console.Beep();
+    }
+
+    {
+    Console.WriteLine("There were no wrong answers...");
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+    Console.Beep();
+    }
+
+    
+        Console.WriteLine("Is the earth flat \n " + "A: Yes \n " + "B: No \n ");
+        String answer3 = Console.ReadLine();
+
+        if (answer3 == "A")
+        {
+            Console.WriteLine("-------------------------- \n " + "Incorrect \n" + "-------------------------- \n ");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Beep();
+        }
+
+        if (answer3 == "B")
+        {
+            Console.WriteLine("-------------------------- \n " + "Correct \n" + "-------------------------- \n ");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Beep();
+        }
+
+        {
+            Console.WriteLine("Thanks for playing the Cup of Linux quiz!!!!");
+            Console.ReadKey();        
+            Console.Beep();    
+        }
+
+
+
+       
+
+        
+
+
+
